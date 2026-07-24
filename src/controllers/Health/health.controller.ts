e29ed94438216c48574101f54ee6d75937c7bca5
janhaviganorkar <janhaviganorkar12@gmail.com>
2026-07-24 15:23:36 +0530

    ready to deploy

--- src/controllers/Health/health.controller.ts
+++ src/controllers/Health/health.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { errorResponse, successResponse } from "@/utils/response";
+import { errorResponse, successResponse } from "@/utils/response.js";
 
 export const healthCheck = async (req: Request, res: Response) => {
   try {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,9 @@ import { Router } from "express";
 import userRouter from "./User/user.route.js";
 import groupRouter from "./Group/group.route.js";
 import currencyRouter from "./Currency/currency.route.js";
-import healthRoutes from "@/routes/Health/health.routes";
+import healthRoutes from "@/routes/Health/health.routes.js";
 import expenseRoutes from "@/routes/Expense/expense.route.js";
-import dashboardRouter from "./dashboard/dashboard.route";
+import dashboardRouter from "./dashboard/dashboard.route.js";
 const router = Router();
 
 router.use("/user", userRouter);
